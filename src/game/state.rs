1a33d19cdf74e584e382674a722e86862e7ea1e5
Joe Fehrman <joseph.fehrman@gmail.com>
2024-01-06 16:54:11 -0500

    chore: Adding documentation to the game state module.

--- src/game/state.rs
+++ src/game/state.rs
@@ -1,8 +1,20 @@
+/// A module that contains the state of the game.
 pub struct GameState {
     pub mode: Mode,
 }
 
 impl GameState {
+    /// A function that creates a new GameState.
+    ///
+    /// # Returns
+    /// * `GameState` - A new GameState.
+    ///
+    /// # Examples
+    /// ```
+    /// use retribution::game::state;
+    ///
+    /// let game_state = state::GameState::new();
+    /// ```
     pub fn new() -> GameState {
         GameState {
             mode: Mode::Travel,
