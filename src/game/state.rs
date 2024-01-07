557fdb4fa98faf46b64a2f162eb142da783cdfc3
Joe Fehrman <joseph.fehrman@gmail.com>
2024-01-06 21:13:24 -0500

    feat: Adding room to game state.

--- src/game/state.rs
+++ src/game/state.rs
@@ -1,12 +1,16 @@
 //! # State
 //! This module contains the state of the game.
+use crate::game::room;
 
 /// A module that contains the state of the game.
-pub struct GameState {
+pub struct GameState<'a> {
+    /// The current mode of the game.
     pub mode: Mode,
+    /// The current room the player is in.
+    pub room: Option<&'a room::Room>,
 }
 
-impl GameState {
+impl<'a> GameState<'a> {
     /// A function that creates a new GameState.
     ///
     /// # Returns
@@ -18,13 +22,19 @@ impl GameState {
     ///
     /// let game_state = state::GameState::new();
     /// ```
-    pub fn new() -> GameState {
+    pub fn new() -> GameState<'a> {
         GameState {
             mode: Mode::Travel,
+            room: None,
         }
     }
+
+    // pub fn set_room(&mut self, room: &'a room::Room) {
+    //    self.room = Some(room);
+    // }
 }
 
+/// An enum that represents the mode of the game.
 pub enum Mode {
     Combat,
     Menu,
